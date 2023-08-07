cddb1f8b6b9929abb91ca51c4875cc49ec8e5b75
Matthew <mpetillo@ku.edu>
2023-08-06 21:35:18 -0700

    adding about-me content

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ function pageup(id){//this takes a single id, the section that was clicked
   menu.classList.add("fullfadeout");
   header.innerHTML="<div class='center fullfadein'><div class='back-button' onclick='pagedown()'>← Go Back</div></div>";
   if (id == 'aboutme'){
-  article.innerHTML = "<div class='center' id='articlegrid'><div class='container article-in' id='articlecontainer'><div id='paper'><div class='article-back article-back-waves'><div class='article-front article-front-waves' id='paper'><h1>TOMFUCKERY</h1></div></div></div></div></div>";
+  article.innerHTML = "<div class='center' id='articlegrid'><div class='container article-in' id='articlecontainer'><div id='paper'><div class='article-back article-back-waves'><div class='article-front article-front-waves' id='paper'><h1>The Front Page</h1><p>Hi there! I’m Matthew Petillo. I’m a junior double majoring in Computer Science and Journalism at the University of Kansas. I’m currently the Editor-In-Chief for the University Daily Kansan, the student newspaper at KU.</p><img src='photoofmeinterviewing-2.jpg' height='500px' width='auto'><p>I’ve been working in student papers for years – first in middle school as an opinion writer, and then in high school as a reporter. I got my professional start working for Lawrence Business Magazine, a local trade publication. When I came to KU, I knew I was going to work for the student paper – because I love doing the news.</p><p>Other than that, I currently work with Professor Alex Bardas, a professor in the Electrical Engineering and Computer Science department at the School of Engineering, in his lab. My goal is to find the intersection of computers and journalism, and build tools for journalists to use to make their reporting better.</p></div></div></div></div></div>  ";
   }
 }
 
@@ -44,5 +44,10 @@ function pagedown(){//in theory, we don't need to use any ids here.
   menu.classList.remove("fullfadeout");
   menu.classList.add("fullfadein");
   header.innerHTML="<h1 class='fullfadein'>The News Stand</h1>"
+}
+
+function versionnum(){
+ const versionhtml = document.getElementById("version");
+
 }
 
